b54054b34568578ce5ab2a043c7e7346b3c7f47f
YounnesYBN <younnesYBN@outlook.com>
2023-03-16 12:29:42 +0100

    finishing all function that calculate the data and store them

--- app/Http/Controllers/OperationController.php
+++ app/Http/Controllers/OperationController.php
@@ -70,10 +70,144 @@ class OperationController extends Controller
         $donne->save();
     }
 
-    public function getNombreTotalGroupesTauxConvenable($code_filier, $annee, $convenable)
+    public function getNombreTotalGroupesTaux($code_filier, $annee, $convenable, $moiyen)
     {
-        // $count = 0;
-        // $total = DB::table("data_tables")->selectRaw("groupe,avg(Taux_Realisation_P_syn)")->where("code_filiere", $code_filier)->where("annee_formation", $annee)->groupBy("groupe")->get();
-        // dd($total);
+        //get total of groups that have taux convonabl and moiyen and faible and also achever
+        $ConMoiFaiARRAY = ["convenable" => 0, "faible" => 0, "moiyen" => 0, "con&moi" => 0];
+        $elements = [
+            ["ele" => 40, "type" => "convenable"],
+            ["ele" => 41, "type" => "moiyen"],
+            ["ele" => 42, "type" => "faible"],
+            ["ele" => 43, "type" => "con&moi"],
+
+        ];
+        // SELECT groupe,CEIL((sum(mh_realisee_globale) /sum(MH_Affectee_Globale_P_SYN))*100) from data_tables
+        // WHERE annee_formation = 1 and code_filiere = "GC_GE_TS"
+        // group by groupe
+
+        $allGroupWithTaux = DB::table("data_tables")->selectRaw("groupe,CEIL((sum(mh_realisee_globale) /sum(MH_Affectee_Globale_P_SYN))*100) as taux")->where("code_filiere", $code_filier)->where("annee_formation", $annee)->groupBy("groupe")->get();
+
+        foreach ($allGroupWithTaux as $key) {
+            if ($key->taux >= $convenable) {
+                $ConMoiFaiARRAY["convenable"]++;
+            } elseif ($key->taux < $convenable && $key->taux >= $moiyen) {
+                $ConMoiFaiARRAY["moiyen"]++;
+            } else {
+                $ConMoiFaiARRAY["faible"]++;
+            }
+        }
+        $ConMoiFaiARRAY["con&moi"] = $ConMoiFaiARRAY["convenable"] + $ConMoiFaiARRAY["moiyen"];
+
+
+        $id_code_filier = Filiere::all()->where("code_filiere", $code_filier)->where("annee", $annee)->first()->id;
+        foreach ($elements as $key) {
+
+            $donne = Donnee::all()->where("filiere_id", $id_code_filier)->where("element_id", $key["ele"])->first();
+
+            if ($donne) {
+                $donne->value = $ConMoiFaiARRAY[$key["type"]];
+            } else {
+                $donne = new Donnee();
+                $donne->value = $ConMoiFaiARRAY[$key["type"]];
+                $donne->filiere_id = $id_code_filier;
+                $donne->element_id = $key["ele"];
+            }
+
+            $donne->save();
+        }
     }
+
+
+    public function getTotalModule($code_filier, $annee)
+    {
+
+        $eleid = 14;
+        $id_code_filier = Filiere::all()->where("code_filiere", $code_filier)->where("annee", $annee)->first()->id;
+
+        $CountModule = DataTable::all()->where("code_filiere", $code_filier)->where("annee_formation", $annee)->count("module");
+
+        $donne = Donnee::all()->where("filiere_id", $id_code_filier)->where("element_id", $eleid)->first();
+
+        if ($donne) {
+            $donne->value = $CountModule;
+        } else {
+            $donne = new Donnee();
+            $donne->value = $CountModule;
+            $donne->filiere_id = $id_code_filier;
+            $donne->element_id = $eleid;
+        }
+
+        $donne->save();
+    }
+
+
+    public function getTotalModuleAchever($code_filier, $annee)
+    {
+        $eleid = 15;
+        $id_code_filier = Filiere::all()->where("code_filiere", $code_filier)->where("annee", $annee)->first()->id;
+
+        $CountModuleAchever = DataTable::all()->where("code_filiere", $code_filier)->where("annee_formation", $annee)->where("Taux_Realisation_P_syn", ">=", 95)->count("module");
+        
+        $donne = Donnee::all()->where("filiere_id", $id_code_filier)->where("element_id", $eleid)->first();
+
+        if ($donne) {
+            $donne->value = $CountModuleAchever;
+        } else {
+            $donne = new Donnee();
+            $donne->value = $CountModuleAchever;
+            $donne->filiere_id = $id_code_filier;
+            $donne->element_id = $eleid;
+        }
+
+        $donne->save();
+    }
+
+    public function  getTotalEFM_local_regional($code_filier, $annee){
+
+        //get total of EFM local and regional in both prevues and realisees
+        $EFMArray = [
+            ["locales prévues"=>0],
+            ["régionales prévues"=>0],
+            ["locales réalisées"=>0],
+            ["régionales réalisées"=>0],
+        ];
+
+        $idElements = [
+            ["id"=>16,"type"=>"locales prévues"],
+            ["id"=>17,"type"=>"régionales prévues"],
+            ["id"=>18,"type"=>"locales réalisées"],
+            ["id"=>19,"type"=>"régionales réalisées"]
+        ];
+        $id_code_filier = Filiere::all()->where("code_filiere", $code_filier)->where("annee", $annee)->first()->id;
+
+        $CountEFMLocalesPrevues = DataTable::all()->where("code_filiere", $code_filier)->where("annee_formation", $annee)->where("Regional","N")->where("Seance_EFM","Non")->count();
+        $CountEFMRegionalPrevues = DataTable::all()->where("code_filiere", $code_filier)->where("annee_formation", $annee)->where("Regional","O")->where("Seance_EFM","Non")->count();
+        $CountEFMLocalesréalisées = DataTable::all()->where("code_filiere", $code_filier)->where("annee_formation", $annee)->where("Regional","N")->where("Seance_EFM","Oui")->count();
+        $CountEFMRegionalréalisées = DataTable::all()->where("code_filiere", $code_filier)->where("annee_formation", $annee)->where("Regional","O")->where("Seance_EFM","Oui")->count();
+
+        $EFMArray["locales prévues"] =$CountEFMLocalesPrevues; 
+        $EFMArray["régionales prévues"] =$CountEFMRegionalPrevues; 
+        $EFMArray["locales réalisées"] =$CountEFMLocalesréalisées; 
+        $EFMArray["régionales réalisées"] =$CountEFMRegionalréalisées; 
+
+
+        foreach ($idElements as $key) {
+            
+            $donne = Donnee::all()->where("filiere_id", $id_code_filier)->where("element_id", $key["id"])->first();
+    
+            if ($donne) {
+                $donne->value = $EFMArray[$key["type"]];
+            } else {
+                $donne = new Donnee();
+                $donne->value = $EFMArray[$key["type"]];
+                $donne->filiere_id = $id_code_filier;
+                $donne->element_id = $key["id"];
+            }
+    
+            $donne->save();
+        }
+        
+       
+    }
+    
 }
--- app/Http/Controllers/uploadController.php
+++ app/Http/Controllers/uploadController.php
@@ -12,6 +12,7 @@ use Illuminate\Routing\Controller;
 use Maatwebsite\Excel\Facades\Excel;
 use App\Imports\dataImport;
 use App\Models\DataTable;
+use App\Models\Filiere;
 
 class uploadController extends Controller
 {
@@ -22,35 +23,37 @@ class uploadController extends Controller
     {
 
         $input = [
-            "file_import"=>$request->file_import,
-            "convenable"=>$request->convenable,
-            "moiyen"=>$request->moiyen,
+            "file_import" => $request->file_import,
+            "convenable" => $request->convenable,
+            "moiyen" => $request->moiyen,
         ];
 
-        Validator::validate($input,[
-            "file_import"=>["required","mimes:xlsx,xls"],
-            "moiyen"=>["required"],
-            "convenable"=>["required"],
+        Validator::validate($input, [
+            "file_import" => ["required", "mimes:xlsx,xls"],
+            "moiyen" => ["required"],
+            "convenable" => ["required"],
         ]);
-        
-        if(DataTable::all()->first()){
+
+        if (DataTable::all()->first()) {
 
             DataTable::truncate();
-            
         }
 
-        
+
         Excel::import(new dataImport, $request->file("file_import")->store("temp"));
         $operationOBJ = new OperationController();
-        // $operationOBJ->getNmbreTotalGroup("GC_GE_TS","1"); //
-        //  $operationOBJ->getNombreTotalGroupesValides("GC_GE_TS","1");
-         return $operationOBJ->getNombreTotalGroupesTauxConvenable("GC_GE_TS","1",$request->convenable);
 
-        // return "nice";
+        $allFilier = Filiere::all();
+        foreach ($allFilier as $filier) {
 
-        
-        
-        
-        
+            $operationOBJ->getNmbreTotalGroup($filier->code_filiere, $filier->annee);
+            $operationOBJ->getNombreTotalGroupesValides($filier->code_filiere, $filier->annee);
+            $operationOBJ->getNombreTotalGroupesTaux($filier->code_filiere, $filier->annee, $request->convenable, $request->moiyen);
+            $operationOBJ->getTotalModule($filier->code_filiere, $filier->annee);
+            $operationOBJ->getTotalModuleAchever($filier->code_filiere, $filier->annee);
+            $operationOBJ->getTotalEFM_local_regional($filier->code_filiere, $filier->annee);
+        }
+
+        return "nice";
     }
 }
